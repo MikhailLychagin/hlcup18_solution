54491bfa06272bf0deccc4392a37e8406d89dff1
Лычагин Михаил Сергеевич <MLychagin@alfabank.ru>
2019-01-10 19:15:05 +0300

    New account handler

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 import asyncio
+import io
+from datetime import datetime
 
 import asyncpg
 from aiohttp import web
@@ -19,13 +21,122 @@ ALLOWED_QUERY_FIELDS = {
     "premium_now", "premium_null",
 }
 
+SPECIAL_ACCOUNT_FIELDS = {
+    "interests", "likes"
+}
+
+STATUS_MAP = {"свободны": 0, "заняты": 1, "всё сложно": 2}
+
 
 async def accounts_filter(request):
-    for k in request.query.keys():
-        if k not in ALLOWED_QUERY_FIELDS:
-            return web.Response(status=400)
+    pass
 
 
+async def accounts_get(request):
+    pass
+
+
+async def accounts_post(request):
+    pass
+
+
+async def accounts_new(request):
+    body = await request.json()
+    columns, values = io.StringIO(), io.StringIO()
+    interests, likes = None, None
+    i = 1
+    for k, v in body.items():
+        if k == "id":
+            continue
+        elif k == "interests":
+            interests = v
+            continue
+        elif k == "likes":
+            likes = v
+            continue
+        elif k == "status":
+            v = STATUS_MAP[v]
+        elif k == "birth":
+            if columns.tell():
+                columns.write(",")
+            columns.write("birth_year")
+
+            parsed_date = datetime.utcfromtimestamp(v)
+
+            if values.tell():
+                values.write(",")
+            values.write(str(parsed_date.year))
+
+        if columns.tell():
+            columns.write(",")
+        columns.write(k)
+
+        if values.tell():
+            values.write(",")
+        if isinstance(v, str):
+            values.write("'")
+            values.write(v)
+            values.write("'")
+        else:
+            values.write(str(v))
+
+        i += 1
+
+    columns.seek(0)
+    values.seek(0)
+
+    async with app['pool'].acquire() as conn:
+        async with conn.transaction():
+            account_id = await conn.fetchval(
+                """INSERT INTO tbl_accounts
+                ({})
+                VALUES ({})
+                RETURNING id;
+                """.format(columns.read(), values.read())
+            )
+
+            if interests:
+                res = await conn.fetch(
+                    """
+                    WITH inserted_interests AS (
+                        INSERT INTO tbl_interests (descr)
+                        (
+                            SELECT 
+                                inp.descr
+                            FROM
+                                UNNEST($1::tbl_interests[]) AS inp
+                        )
+                        ON CONFLICT ON CONSTRAINT tbl_interests_descr_key DO NOTHING
+                        RETURNING id
+                    )
+                    SELECT id
+                    FROM inserted_interests
+                    UNION
+                    SELECT id
+                    FROM tbl_interests
+                    WHERE descr = any($2::varchar[]);
+                    """,
+                    tuple((None, i,) for i in interests),
+                    tuple(i for i in interests)
+                )
+                interests_ids = ((account_id, record["id"]) for record in res)
+                await conn.executemany(
+                    """INSERT INTO accounts_interests (account_id, interest_id)
+                    VALUES ($1, $2)
+                    """,
+                    interests_ids,
+                )
+
+            if likes:
+                await conn.executemany(
+                    """INSERT INTO tbl_likes
+                    (account_id, liked_account_id, ts)
+                    VALUES ($1, $2, $3);
+                    """,
+                    ((account_id, i["id"], i["ts"]) for i in likes)
+                )
+
+    return web.Response(status=201)
 
 
 async def init_app():
@@ -34,9 +145,12 @@ async def init_app():
                                             port=15432,
                                             user='postgres',
                                             password='password',
+                                            database='pairer',
                                             ssl=False)
-    app.add_routes([web.get('/accounts/filter/', accounts_filter)])
+    app.add_routes([web.post('/accounts/new/', accounts_new)])
+
+    return app
 
 loop = asyncio.get_event_loop()
 app = loop.run_until_complete(init_app())
-web.run_app(app)
+web.run_app(app, port=28080)
